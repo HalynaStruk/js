734cf8ec8daa9109239dfee11f573135b7f278cc
struk <halynkastru@gmail.com>
2022-02-08 22:52:48 +0200

    classwork 3

--- lesson 3/classwork3.js
+++ lesson 3/classwork3.js
@@ -39,7 +39,61 @@ for (let n = 0; n < arrs.length; n++) {
     }
 }
 // 7. замінити кожне число кратне 3 на слово "okten"
-
+for (g = 0; g < arrs.length; g++) {
+    if (arrs[g]%3 === 0) {
+        arrs[g] = 'okten';
+        console.log(arrs[g]);
+    }
+}
+console.log(arrs);
 // 8. вивести масив в зворотньому порядку.
-
+for (let m = arrs.length-1; m >= 0; m--) {
+    document.write(arrs[m]+'<br>');
+}
 // 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+                        // 1.
+let i1 = arrs.length-1;
+while (i1>= 0) {
+    console.log(arrs[i1]);
+    i1--
+}
+                        // 2.
+for (let arr = arrs.length-1; arr >= 0; arr--) {
+    document.write(arrs[arr]+'<br>');
+}
+                        // 3.
+let x3 = arrs.length-1;
+while (x3>=0) {
+    if (x3%2 === 1){
+        console.log(arrs[x3]);
+    }
+    x3--
+}
+                        // 4.
+for (let j = arrs.length-1; j >= 0; j--) {
+    if (j%2 === 1) {
+        console.log(arrs[j]);
+    }
+}
+                        // 5.
+let h5 = arrs.length-1;
+while (h5>=0) {
+    if (h5%2 === 0) {
+        console.log(arrs[h5]);
+    }
+    h5--
+}
+                        // 6.
+for (let n = arrs.length-1; n >= 0; n--) {
+    if (n%2 === 0) {
+        console.log(arrs[n]);
+    }
+}
+                        // 7.
+for (g = arrs.length-1; g >= 0; g--) {
+    if (arrs[g]%3 === 0) {
+        arrs[g] = 'okten';
+        console.log(arrs[g]);
+    }
+}
+console.log(arrs);
