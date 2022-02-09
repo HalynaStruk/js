e951a2c743ab811afdd3bf4f3115a3fc0deaef24
struk <halynkastru@gmail.com>
2022-02-09 19:54:50 +0200

    homework 4

--- lesson 4/classwork4.js
+++ lesson 4/classwork4.js
@@ -1,19 +1,85 @@
 // 1. створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
-
+function numbermin(a,b,c) {
+    if (a<b && a<c) {
+        document.write(a +'<br>');
+    } else if (b<a && b<c) {
+        document.write(b +'<br>');
+    } else if (c<a && c<b) {
+        document.write(c +'<br>');
+    }
+}
+numbermin(3,12,9);
 // 2. створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
-
+function numbermax(x,y,z) {
+    if (x>y && x>z) {
+        document.write(x +'<br>');
+    } else if (y>x && y>z) { +'<br>'
+        document.write(y +'<br>');
+    } else if (z>x && z>y) {
+        document.write(z +'<br>');
+    }
+}
+numbermax(3,12,9);
 // 3. створити функцію яка повертає найбільше число з масиву
-
+let arrayMax = [125,1,3,6,9,12,18,21];
+function arr(arrayMax) {
+    for (let i = 0; i < arrayMax.length; i++) {
+        if (arrayMax[i] > arrayMax[0]) {
+            arrayMax[0] = arrayMax[i];
+        }
+    }
+    return arrayMax[0];
+}
+document.write(arr(arrayMax) + '<br>');
 // 4. створити функцію яка повертає найменьше число з масиву
-
+let arrayMin = [1,3,6,9,125,12,18,21];
+function arrmin(arrayMin) {
+    for (let i = 0; i < arrayMin.length; i++) {
+         if (arrayMin[i] < arrayMin[0]) {
+             arrayMin[0] = arrayMin[i];
+         }
+    }
+    return arrayMin[0];
+}
+document.write(arrmin(arrayMin) + '<br>');
 // 5. створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 // Приклад [1,2,10]->13
-
+let arrnumbers = [25,50,35,15];
+function funarr(arrnumbers) {
+    let sum = 0;
+    for (let i = 0; i < arrnumbers.length; i++) {
+       sum = sum + arrnumbers[i];
+    }
+    return sum;
+}
+document.write(funarr(arrnumbers) + '<br>');
 // 6. створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-
+let numbers = [30,60,90,120];
+function arithmetic(numbers) {
+    let sum1 = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum1 = sum1 + numbers[i];
+    }
+    return sum1/numbers.length;
+}
+document.write(arithmetic(numbers) + '<br>');
 // 7. створити функцію яка приймає будь-яку кількість чисел, повертає найменьше,
 // а виводить найбільше (Math використовувати заборонено);
-
+function retMinConsMax(anynumbers) {
+    let min = anynumbers[0];
+    let max = anynumbers[0];
+    for (let i = 0; i < anynumbers.length; i++) {
+        if (anynumbers[i] > max) {
+            anynumbers[i] = max;
+        }
+        if (anynumbers[i] < min) {
+            anynumbers[i] = min;
+        }
+    }
+    console.log(max);
+    return min;
+}
+document.write(retMinConsMax([5,15,3,27]) + '<br>');
 // 8. створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
 
