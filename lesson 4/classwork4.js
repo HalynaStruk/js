e9bc5cfe8551af5d995b95a55f5f7fc8757afb7b
struk <halynkastru@gmail.com>
2022-02-11 15:42:13 +0200

    classwork 4

--- lesson 4/classwork4.js
+++ lesson 4/classwork4.js
@@ -21,7 +21,6 @@ function numbermax(x,y,z) {
 }
 numbermax(3,12,9);
 // 3. створити функцію яка повертає найбільше число з масиву
-let arrayMax = [125,1,3,6,9,12,18,21];
 function arr(arrayMax) {
     for (let i = 0; i < arrayMax.length; i++) {
         if (arrayMax[i] > arrayMax[0]) {
@@ -30,9 +29,9 @@ function arr(arrayMax) {
     }
     return arrayMax[0];
 }
-document.write(arr(arrayMax) + '<br>');
+document.write(arr([125,1,3,6,9,12,18,21]) + '<br>');
+
 // 4. створити функцію яка повертає найменьше число з масиву
-let arrayMin = [1,3,6,9,125,12,18,21];
 function arrmin(arrayMin) {
     for (let i = 0; i < arrayMin.length; i++) {
          if (arrayMin[i] < arrayMin[0]) {
@@ -41,7 +40,7 @@ function arrmin(arrayMin) {
     }
     return arrayMin[0];
 }
-document.write(arrmin(arrayMin) + '<br>');
+document.write(arrmin([1,3,6,9,125,12,18,21]) + '<br>');
 // 5. створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 // Приклад [1,2,10]->13
 let arrnumbers = [25,50,35,15];
@@ -66,35 +65,21 @@ document.write(arithmetic(numbers) + '<br>');
 // 7. створити функцію яка приймає будь-яку кількість чисел, повертає найменьше,
 // а виводить найбільше (Math використовувати заборонено);
 
-// function retMinConsMax(anynumbers) {
-//     for (let i = 0; i < anynumbers.length; i++) {
-//         if (anynumbers[i] > anynumbers[0]) {
-//             anynumbers[i] = max;
-//         }
-//         if (anynumbers[i] < anynumbers[0]) {
-//             anynumbers[i] = min;
-//         }
-//     }
-//     console.log(max);
-//     return min;
-// }
-// document.write(retMinConsMax([5,15,3,27]) + '<br>');
-
-// function minMax(arg) {
-//     let min = arguments[0];
-//     let max = arguments[0];
-//     for (const element of arguments) {
-//         if (element > max) {
-//             max = element;
-//         }
-//         if (element < min) {
-//             min = element;
-//         }
-//     }
-//     console.log("max ", max);
-//     return min;
-// }
-// document.write('Мінімальне число з введених: ' + minMax(100, 20, 3, 100, 2021))
+function retMinConsMax(anynumbers) {
+    let max = arguments[0];
+    let min = arguments[0];
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > arguments[0]) {
+            max = arguments[i];
+        }
+        if (arguments[i] < arguments[0]) {
+            min = arguments[i];
+        }
+    }
+    console.log('Максимальне число',max);
+    return min;
+}
+document.write('Мінімальне число '+ retMinConsMax(5,15,3,27) + '<br>');
 // 8. створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
 
@@ -119,16 +104,25 @@ function generateslim(quantity,limit) {
 }
 generateslim(15,400);
 // 10. Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
-function generates(quantity) {
-    let arr = [];
-    for (let i = 0; i < quantity; i++) {
-        g = Math.round(Math.random()*100);
-        arr.push(g);
-    }
-    console.log(arr);
-    arr[i] = arr[k];
+function reverse(arr) {
+    let revArray = [];
     for (let k = arr.length-1; k >=0 ; k--) {
+        revArray.push(arr[k]);
     }
     console.log(arr);
+    console.log(revArray);
 }
-generatesran(5);
+reverse([1,2,3,4,5]);
+
+function reverse1(arr1) {
+    let revarr = [];
+    let ri = arr1.length-1;
+    for (let i = 0; i < arr1.length; i++) {
+        revarr[ri] = arr1[i];
+        ri--;
+    }
+    console.log(arr1);
+    console.log(revarr);
+}
+reverse1([1,2,3]);
+
