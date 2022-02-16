f3aa14ba83eec8f7f6e4d318aa121994cba0d97d
struk <halynkastru@gmail.com>
2022-02-16 12:13:44 +0200

    classwork 6

--- lesson 6/classwork6.js
+++ lesson 6/classwork6.js
@@ -79,3 +79,16 @@ console.log(sortNums('ascending',[3,57,21,35,95,37,48,-6,149]));
 // ];
 // -- відсортувати його за спаданням за monthDuration
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+// -- відсортувати його за спаданням за monthDuration
+console.log(coursesAndDurationArray.sort((first,second) => second.monthDuration - first.monthDuration));
+
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+console.log(coursesAndDurationArray.filter(duration => duration.monthDuration > 5));
