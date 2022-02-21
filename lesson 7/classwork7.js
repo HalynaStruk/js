e22a86995a5ac4adc560d3b37b102639ac6a6346
struk <halynkastru@gmail.com>
2022-02-21 19:22:03 +0200

    classwork 7

--- lesson 7/classwork7.js
+++ lesson 7/classwork7.js
@@ -16,9 +16,7 @@ function Cars(model, manufacturer, year_of_manufacture, maximum_speed, engine_ca
         console.log(`їдемо зі швидкістю ${maximum_speed} км на годину`)
     };
     this.info = function () {
-        for (const car of Cars) {
-            console.log(Car);
-        }
+
     };
    this.increaseMaxSpeed = function (newSpeed) {
         maximum_speed = maximum_speed + newSpeed;
@@ -38,6 +36,8 @@ function Cars(model, manufacturer, year_of_manufacture, maximum_speed, engine_ca
 let car1 = new Cars('BMW', 'Germany',2009,230,3.0);
 console.log(car1);
 car1.drive();
+console.log(car1.info());
+
 car1.increaseMaxSpeed(40);
 car1.changeYea(2010);
 car1.addDriver('Ivan','КВІ304050');
@@ -51,22 +51,80 @@ car1.addDriver('Ivan','КВІ304050');
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів,
 // і додає його в поточний об'єкт car
-// class Car2 {
-//     constructor(model, manufacturer, year_of_manufacture, maximum_speed, engine_capacity) {
-//         this.model = model;
-//         this.manufacturer = manufacturer;
-//         this.year_of_manufacture = year_of_manufacture;
-//         this.maximum_speed = maximum_speed;
-//         this.engine_capacity = engine_capacity;
-//     }
-// }
+class Cars2 {
+    constructor(model, manufacturer, year_of_manufacture, maximum_speed, engine_capacity) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.year_of_manufacture = year_of_manufacture;
+        this.maximum_speed = maximum_speed;
+        this.engine_capacity = engine_capacity;
+    }
+    drive () {
+        console.log(`їдемо зі швидкістю ${maximum_speed} км на годину`);
+    };
+    info () {
+        // return {`Id - ${this.Id}, Name - &{this.Name}`};
+
+    };
+    increaseMaxSpeed (newSpeed) {
+        this.maximum_speed = this.maximum_speed + newSpeed;
+        console.log(this.maximum_speed);
+    };
+    changeYear (newValue) {
+        this.year_of_manufacture = newValue;
+        console.log(this.year_of_manufacture);
+    };
+    addDriver (driver,drivers_license) {
+        this.driver = driver;
+        this.drivers_license = drivers_license;
+        console.log(driver);
+        console.log(drivers_license);
+    }
+}
+let car2 = new Cars2('Audi', 'Germany',2019,240,3.0);
+console.log(car2);
+car2.increaseMaxSpeed(10);
+car2.changeYear(2020);
+car2.addDriver('max','КВТ356287');
 // 3. створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги.
 // Створити масив з 10 попелюшок.
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
 // За допомоги циклу знайти яка попелюшка повинна бути з принцом.
 // Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+function Cinderella(name, age, foot_size) {
+    this.name = name;
+    this.age = age;
+    this.foot_size = foot_size;
+}
+let Cinderellas =[
+    new Cinderella('ira',20,35),
+    new Cinderella('olya',22,38),
+    new Cinderella('sasha',27,39),
+    new Cinderella('inna',39,36),
+    new Cinderella('julia',24,35),
+    new Cinderella('Anna',29,37),
+    new Cinderella('vika',40,36),
+    new Cinderella('maria',25,38),
+    new Cinderella('daria',21,40),
+    new Cinderella('yana',43,36),
+];
+class Prince {
+    constructor(name, age, shoe) {
+        this.name = name;
+        this.age = age;
+        this.shoe = shoe;
+    }
+}
 
-
+let Princes = new Prince('Max',35,37)
+function CinderellasPrince (Cinderellas, Prince) {
+    for (let i = 0; i < Cinderellas.length; i++) {
+       if (Cinderellas[i].foot_size === Prince.shoe) {
+           return `${Cinderellas[i].name} повинна бути з принцом, бо її розмір ноги ${Cinderellas[i].foot_size}, принц знайшов туфельку розміру ${Prince.shoe}`;
+       }
+    }
+}
+console.log(CinderellasPrince (Cinderellas, Princes));
 
 
 // let user = {
