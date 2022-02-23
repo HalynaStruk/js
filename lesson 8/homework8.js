42d78799424b09b127429263b2b953d7e640f068
struk <halynkastru@gmail.com>
2022-02-23 13:56:23 +0200

    homework 8

--- lesson 8/homework8.js
+++ lesson 8/homework8.js
@@ -16,11 +16,25 @@ content1.innerText = 'Hello';
 // 4. замініть текст параграфа з id 'rules' на будь-який інший
 content2.innerHTML = '<i>This is lesson 8 with javascript</i>';
 // 5. змініть кожному елементу колір фону на червоний
-
+    content1.style.background = 'red';
+    content2.style.background = 'red';
+    console.log(document.body.children);
+    let ulBlock = document.body.children[2];
+    ulBlock.style.background = 'red';
 // 6. змініть кожному елементу колір тексту на синій
-
+content1.style.color = 'blue';
+content2.style.color = 'blue';
+ulBlock.style.color = 'blue';
 // 7. отримати весь список класів елемента з id=rules і вивести їх в console.log
-
+let classListDiv = document.getElementById("rules");
+console.log(classListDiv.classList);
 // 8. поміняти колір тексту у всіх елементів fc_rules на червоний
-
-
+let elements = document.getElementsByClassName('fc_rules');
+console.log(elements)
+elements[0].style.color = '#dc0404';
+elements[1].style.color = '#dc0404';
+elements[2].style.color = '#dc0404';
+elements[3].style.color = '#dc0404';
+                            //або
+let elemColor = document.body.children[2];
+elemColor.style.color = '#dc0404';
