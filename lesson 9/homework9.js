0c4b4d4fe19ef297322699945b5ca0524e20d042
struk <halynkastru@gmail.com>
2022-03-14 20:29:57 +0100

    classwork 9

--- lesson 9/homework9.js
+++ lesson 9/homework9.js
@@ -1,15 +1,32 @@
 // Все робити за допомоги js.
 // - створити блок,
-//     - додати йому класи wrap, collapse, alpha, beta
+let bloc = document.createElement('div');
+// - додати йому класи wrap, collapse, alpha, beta
+bloc.classList.add('wrap');
+bloc.classList.add('collapse');
+bloc.classList.add('alpha');
+bloc.classList.add('beta');
 // - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+bloc.style.background = 'blue';
+bloc.style.color = 'yellow';
+bloc.style.fontSize = '25px';
 // - додати цей блок в body.
+document.body.appendChild(bloc);
 // - клонувати його повністю, та додати клон в body.
-//
+let cloneBloc = bloc.cloneNode(true);
+document.body.appendChild(cloneBloc);
 // - Є масив:
 //     ['Main','Products','About us','Contacts']
-// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+// Взяти файл template1.html та додати в нього скріпт
+// котрий для кожного елементу масиву створює li та додає його до блоку .menu
 // Завдання робити через цикли.
-//
+let arrays = ['Main','Products','About us','Contacts'];
+let menu = document.getElementsByClassName('menu')[0];
+for (let array of arrays) {
+    let li = document.createElement('li');
+    menu.appendChild(li);
+    li.innerText = array;
+}
 // - Є масив
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
