3b4082ea7f4e18deb51eab799cb2d2673480ac8d
struk <halynkastru@gmail.com>
2022-03-24 18:10:27 +0100

    classwork 10

--- lesson 10/classwork10.js
+++ lesson 10/classwork10.js
@@ -49,8 +49,62 @@ button1.onclick = function () {
 // другий - кількість ячеєк, третій вмиіст ячеєк.
 // При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 // (Додатковачастина для завдання)
-//
-//
+let div2 = document.createElement('div');
+document.body.appendChild(div2);
+div2.style.display = 'flex';
+div2.style.alignItems = 'end';
+div2.style.columnGap = '10px';
+div2.style.margin = '10px';
+
+let divInput1 = document.createElement('div');
+let p1 = document.createElement('p');
+p1.innerText = 'Кількість рядків';
+let input1 = document.createElement('input');
+divInput1.append(p1,input1);
+let divInput2 = document.createElement('div');
+let p2 = document.createElement('p');
+p2.innerText = 'Кількість ячейок';
+let input2 = document.createElement('input');
+divInput2.append(p2,input2);
+let divInput3 = document.createElement('div');
+let p3 = document.createElement('p');
+p3.innerText = 'Вміст ячейок';
+let input3 = document.createElement('input');
+divInput3.append(p3,input3);
+let button2 = document.createElement('button');
+button2.innerText = 'Form table';
+button2.style.background = 'yellow';
+button2.style.border = 'solid 3px #00BFFF';
+button2.style.borderRadius = '5px';
+button2.style.height = '30px';
+div2.append(divInput1, divInput2, divInput3, button2);
+
+button2.onclick = function () {
+    let tr = input1.value;
+    let td = input2.value;
+    let contents = input3.value;
+function table(row, column, txt) {
+let div = document.createElement('div');
+document.body.appendChild(div);
+let table = document.createElement('table');
+div.appendChild(table);
+table.style.border = '3px solid black';
+    for (let i = 0; i < row; i++) {
+        let row = document.createElement('tr');
+        row.style.border = '2px solid #00BFFF';
+
+    for (let y = 0; y < column; y++) {
+        let column = document.createElement('td');
+        table.appendChild(row);
+        row.appendChild(column);
+        column.style.border = '2px solid yellow';
+        column.innerText = `${txt}`;
+    }
+    }
+}
+    table(tr, td, contents);
+}
+
 // - Сворити масив не цензцрних слів.
 // Сворити інпут текстового типу.
 // Якщо людина вводить слово і воно міститься в масиві не цензурних слів кинути алерт з попередженням.
