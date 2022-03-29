bb263c6ad6cab41929511a114f62ebf22c45f355
struk <halynkastru@gmail.com>
2022-03-29 21:27:46 +0200

    homework 11

--- lesson 11/homework11.js
+++ lesson 11/homework11.js
@@ -9,30 +9,26 @@ btn.onclick = function () {
         name: nameInput.value,
         age: ageInput.value
     };
-    localStorage.setItem('obj', JSON.stringify(obj));
+    localStorage.setItem('objKey', JSON.stringify(obj));
 };
 // -створити форму з інпутами для model,type та volume автівки.
 //  при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
 let btn2 = document.getElementById('btn2');
 let modelInput = document.getElementById('model');
 let typeInput = document.getElementById('type');
 let volumeInput = document.getElementById('volume');
-let cars = [];
-    btn2.onclick = function () {
-        let car = {
+btn2.onclick = function () {
+            let car = {
             model: modelInput.value,
             type: typeInput.value,
             volume: volumeInput.value
         };
-          localStorage.setItem('carsKey', JSON.stringify(car));
-        //   localStorage.getItem('carsKey');
-        // let newCar = JSON.parse(car);
-        // cars.push(newCar);
+    let cars = JSON.parse(localStorage.getItem('carsKey'));
+    if (!cars) {
+        localStorage.setItem('carsKey', JSON.stringify([car]));
+    } else {
+        cars.push(car);
+        localStorage.setItem('carsKey', JSON.stringify(cars));
     }
-// localStorage.setItem('carsKey', JSON.stringify(car));
-// localStorage.getItem('carsKey');
-// let newCar = JSON.parse(car);
-localStorage.setItem('carsKey', JSON.stringify(car));
-
-cars.push(JSON.parse(localStorage.getItem('carsKey')));
-console.log(cars);
+}
