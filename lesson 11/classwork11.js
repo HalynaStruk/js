d497c4da7629596a446555fe74597b98edb69545
struk <halynkastru@gmail.com>
2022-03-29 17:55:48 +0200

    classwork 11

--- lesson 11/classwork11.js
+++ lesson 11/classwork11.js
@@ -1,18 +1,43 @@
 // є масив -
-// let users = [
-//     {name: 'vasya', age: 31, status: false},
-//     {name: 'petya', age: 30, status: true},
-//     {name: 'kolya', age: 29, status: true},
-//     {name: 'olya', age: 28, status: false},
-//     {name: 'max', age: 30, status: true},
-//     {name: 'anya', age: 31, status: false},
-//     {name: 'oleg', age: 28, status: false},
-//     {name: 'andrey', age: 29, status: true},
-//     {name: 'masha', age: 30, status: true},
-//     {name: 'olya', age: 31, status: false},
-//     {name: 'max', age: 31, status: true}
-// ];
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
 // створити під кожен об'єкт свій блок з конопкою "додати до улюблених" при натисканні на яку
 // об'єкт потрапляє до масиву favorites улюблених обраних об'єктів в локальному сховищі.
 // Створити сторніку favorites.html при переході на яку потрібно вивест в документ
 // всіх обраних на попередньому етапі.
+
+for (const user of users) {
+    let divUser = document.createElement('div');
+    divUser.innerText = `Name: ${user.name}, Age: ${user.age}, Status: ${user.status}`;
+    document.body.append(divUser);
+
+    let addToFavoritesButton = document.createElement('button');
+    addToFavoritesButton.innerText = 'add to favorites';
+    divUser.append(addToFavoritesButton);
+
+    addToFavoritesButton.onclick = function () {
+        let users = localStorage.getItem('favorites'); // JSON string
+        console.log(users);
+        if (!users) {
+        users = [];
+        users.push(user);
+        let stringify = JSON.stringify(users);
+        localStorage.setItem('favorites', stringify);
+        } else {
+            let favoritesArray = JSON.parse(users);
+            favoritesArray.push(user);
+            localStorage.setItem('favorites', JSON.stringify(favoritesArray));
+        }
+    }
+}
--- lesson 11/homework11.js
+++ lesson 11/homework11.js
@@ -1,5 +1,38 @@
 // -створити форму з інпутами для name та age.
 //     При відправці форми записати об'єкт в localstorage
-//
+let btn = document.getElementById('btn');
+let nameInput = document.getElementById('name');
+let ageInput = document.getElementById('age');
+
+btn.onclick = function () {
+    let obj = {
+        name: nameInput.value,
+        age: ageInput.value
+    };
+    localStorage.setItem('obj', JSON.stringify(obj));
+};
 // -створити форму з інпутами для model,type та volume автівки.
-//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+//  при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+let btn2 = document.getElementById('btn2');
+let modelInput = document.getElementById('model');
+let typeInput = document.getElementById('type');
+let volumeInput = document.getElementById('volume');
+let cars = [];
+    btn2.onclick = function () {
+        let car = {
+            model: modelInput.value,
+            type: typeInput.value,
+            volume: volumeInput.value
+        };
+          localStorage.setItem('carsKey', JSON.stringify(car));
+        //   localStorage.getItem('carsKey');
+        // let newCar = JSON.parse(car);
+        // cars.push(newCar);
+    }
+// localStorage.setItem('carsKey', JSON.stringify(car));
+// localStorage.getItem('carsKey');
+// let newCar = JSON.parse(car);
+localStorage.setItem('carsKey', JSON.stringify(car));
+
+cars.push(JSON.parse(localStorage.getItem('carsKey')));
+console.log(cars);
