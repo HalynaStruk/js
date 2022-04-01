994ad88ec35dbb31d57351d885dbc9fb19ca431d
struk <halynkastru@gmail.com>
2022-04-01 16:26:38 +0200

    classwork 12

--- lesson 12/classwork12.js
+++ lesson 12/classwork12.js
@@ -3,3 +3,59 @@
 // https://jsonplaceholder.typicode.com/posts
 // зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок
 // всі коментарі поточного поста
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {return response.json();})
+    .then(postsData => {
+        let divGeneral = document.createElement('div');
+        divGeneral.classList.add('grid-container');
+        document.body.appendChild(divGeneral);
+
+        for (const post of postsData) {
+            let divElement = document.createElement('div');
+            divElement.innerHTML = `
+            <div><h1>${post.id}. ${post.title}</h1>
+            <p><b>body:</b> ${post.body}</p></div>
+<!--            <div><button class="btn">Comments</button><div class="commentHide"></div></div>-->
+            `;
+            divElement.classList.add('grid-item');
+            divGeneral.append(divElement);
+
+            let divBtn = document.createElement('div');
+            divElement.appendChild(divBtn);
+
+            let btn = document.createElement('button');
+            btn.innerText = 'Comments';
+            btn.classList.add('btn');
+            divBtn.appendChild(btn);
+
+            let divComent = document.createElement('div');
+            divBtn.appendChild(divComent);
+
+            btn.onclick = function () {
+                divComent.classList.toggle('hidden');
+                fetch('https://jsonplaceholder.typicode.com/comments')
+                    .then(response => response.json())
+                    .then(commentsData => {
+                        for (const comment of commentsData) {
+                            if (comment.id === post.id) {
+
+                                let commentElement = document.createElement('div');
+                                commentElement.classList.add('elementComment');
+                                divElement.appendChild(divComent);
+                                divComent.appendChild(commentElement);
+
+                                commentElement.innerHTML = `
+                                        <h1>id: ${comment.id}</h1>
+                                        <h1>name: ${comment.name}</h1>
+                                        <h3>email: ${comment.email}</h3>
+                                        <p>body: ${comment.body}</p>
+                                        `;
+                            }
+
+                        }
+
+                    })
+
+        }
+        }
+    })
--- lesson 12/homework12.js
+++ lesson 12/homework12.js
@@ -11,13 +11,13 @@ fetch('https://jsonplaceholder.typicode.com/posts')
     })
     .then(postsDate => {
         let divGeneral = document.createElement('div');
-        document.body.appendChild(divGeneral);
         divGeneral.classList.add('grid-container');
+        document.body.appendChild(divGeneral);
 
         for (const post of postsDate) {
             let divElement = document.createElement('div');
-            divGeneral.appendChild(divElement);
             divElement.classList.add('grid-item');
+            divGeneral.appendChild(divElement);
 
             let h1 = document.createElement('h1');
             h1.innerText = post.id;
@@ -32,11 +32,32 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 
         }
     });
-
-
-
 // 2.
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті.
 // Для кожного елементу свій блок div.comment
 // Всі характеристики повинні мати свої блоки всередені div.comment
 // https://jsonplaceholder.typicode.com/comments
+fetch('https://jsonplaceholder.typicode.com/comments')
+.then(response => response.json())
+.then(commentsData => {
+    let div = document.createElement('div');
+    div.classList.add('container');
+    document.body.appendChild(div);
+
+    for (const comment of commentsData) {
+        let commentElement = document.createElement('div');
+        commentElement.classList.add('elementComment');
+        div.appendChild(commentElement);
+
+        let h1Coment = document.createElement('h1');
+        h1Coment.innerText = `${comment.id}. ${comment.name}`;
+
+        let h3Coment = document.createElement('h3');
+        h3Coment.innerText = `email: ${comment.email}`;
+
+        let pComent = document.createElement('p');
+        pComent.innerHTML = `<b>body:</b> ${comment.body}`;
+
+        commentElement.append(h1Coment, h3Coment, pComent);
+    }
+})
